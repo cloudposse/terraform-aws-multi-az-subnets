f99bc6ca5b32d64fbe59a99539ac2eae18807268
Mike Crowe <mcrowe@zipiitwireless.com>
2018-06-03 06:20:32 -0400

    Updating null-label to 0.3.1 to handle being disabled

--- public.tf
+++ public.tf
@@ -4,7 +4,7 @@ locals {
 }
 
 module "public_label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.0"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.1"
   namespace  = "${var.namespace}"
   name       = "${var.name}"
   stage      = "${var.stage}"
