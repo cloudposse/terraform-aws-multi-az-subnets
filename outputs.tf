b11663d4d64ffca56e6f11b073011c6b7ba1b2c7
Phil Winder <phil@winderresearch.com>
2018-09-24 11:46:40 +0100

    Remove references to tags in outputs
    The AZ subnets are indexed by the count parameter. Since the count parameter is the same for all resources the ordering will be maintained. Hence the double check for AZ tags is redundant.

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "az_subnet_ids" {
-  value       = "${zipmap(var.availability_zones, matchkeys(coalescelist(aws_subnet.private.*.id, aws_subnet.public.*.id), coalescelist(aws_subnet.private.*.tags.AZ, aws_subnet.public.*.tags.AZ), var.availability_zones))}"
+  value       = "${zipmap(var.availability_zones, coalescelist(aws_subnet.private.*.id, aws_subnet.public.*.id))}"
   description = "Map of AZ names to subnet IDs"
 }
 
 output "az_route_table_ids" {
-  value       = "${zipmap(var.availability_zones, matchkeys(coalescelist(aws_route_table.private.*.id, aws_route_table.public.*.id), coalescelist(aws_route_table.private.*.tags.AZ, aws_route_table.public.*.tags.AZ), var.availability_zones))}"
+  value       = "${zipmap(var.availability_zones, coalescelist(aws_route_table.private.*.id, aws_route_table.public.*.id))}"
   description = " Map of AZ names to Route Table IDs"
 }
 
 output "az_ngw_ids" {
-  value       = "${zipmap(var.availability_zones, coalescelist(matchkeys(aws_nat_gateway.public.*.id, aws_nat_gateway.public.*.tags.AZ, var.availability_zones), local.dummy_az_ngw_ids))}"
+  value       = "${zipmap(var.availability_zones, coalescelist(aws_nat_gateway.public.*.id, local.dummy_az_ngw_ids))}"
   description = "Map of AZ names to NAT Gateway IDs (only for public subnets)"
 }
