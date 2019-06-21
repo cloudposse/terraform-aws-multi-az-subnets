d1cce2c8415c5fbbca2a500838245aa1de236943
rverma-nikiai <rohit.verma@niki.ai>
2019-06-21 16:38:32 +0530

    upgraded-tf

--- variables.tf
+++ variables.tf
@@ -1,38 +1,38 @@
 variable "namespace" {
   description = "Namespace (e.g. `cp` or `cloudposse`)"
-  type        = "string"
+  type        = string
 }
 
 variable "stage" {
   description = "Stage (e.g. `prod`, `dev`, `staging`)"
-  type        = "string"
+  type        = string
 }
 
 variable "name" {
-  type        = "string"
+  type        = string
   description = "Application or solution name"
 }
 
 variable "delimiter" {
-  type        = "string"
+  type        = string
   default     = "-"
   description = "Delimiter to be used between `namespace`, `stage`, `name` and `attributes`"
 }
 
 variable "attributes" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "Additional attributes (e.g. `policy` or `role`)"
 }
 
 variable "tags" {
-  type        = "map"
+  type        = map(string)
   default     = {}
   description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)"
 }
 
 variable "availability_zones" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "List of Availability Zones (e.g. `['us-east-1a', 'us-east-1b', 'us-east-1c']`)"
 }
@@ -43,48 +43,48 @@ variable "max_subnets" {
 }
 
 variable "type" {
-  type        = "string"
+  type        = string
   default     = "private"
   description = "Type of subnets to create (`private` or `public`)"
 }
 
 variable "vpc_id" {
-  type        = "string"
+  type        = string
   description = "VPC ID"
 }
 
 variable "cidr_block" {
-  type        = "string"
+  type        = string
   description = "Base CIDR block which is divided into subnet CIDR blocks (e.g. `10.0.0.0/16`)"
 }
 
 variable "igw_id" {
-  type        = "string"
+  type        = string
   description = "Internet Gateway ID that is used as a default route when creating public subnets (e.g. `igw-9c26a123`)"
   default     = ""
 }
 
 variable "az_ngw_ids" {
-  type        = "map"
+  type        = map(string)
   description = "Only for private subnets. Map of AZ names to NAT Gateway IDs that are used as default routes when creating private subnets"
   default     = {}
 }
 
 variable "public_network_acl_id" {
-  type        = "string"
+  type        = string
   description = "Network ACL ID that is added to the public subnets. If empty, a new ACL will be created"
   default     = ""
 }
 
 variable "private_network_acl_id" {
-  type        = "string"
+  type        = string
   description = "Network ACL ID that is added to the private subnets. If empty, a new ACL will be created"
   default     = ""
 }
 
 variable "public_network_acl_egress" {
   description = "Egress network ACL rules"
-  type        = "list"
+  type        = list(string)
 
   default = [
     {
@@ -100,7 +100,7 @@ variable "public_network_acl_egress" {
 
 variable "public_network_acl_ingress" {
   description = "Egress network ACL rules"
-  type        = "list"
+  type        = list(string)
 
   default = [
     {
@@ -116,7 +116,7 @@ variable "public_network_acl_ingress" {
 
 variable "private_network_acl_egress" {
   description = "Egress network ACL rules"
-  type        = "list"
+  type        = list(string)
 
   default = [
     {
@@ -132,7 +132,7 @@ variable "private_network_acl_egress" {
 
 variable "private_network_acl_ingress" {
   description = "Egress network ACL rules"
-  type        = "list"
+  type        = list(string)
 
   default = [
     {
@@ -160,3 +160,4 @@ variable "az_ngw_count" {
   description = "Count of items in the `az_ngw_ids` map. Needs to be explicitly provided since Terraform currently can't use dynamic count on computed resources from different modules. https://github.com/hashicorp/terraform/issues/10857"
   default     = 0
 }
+
