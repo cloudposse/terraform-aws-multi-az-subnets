293a74da05e3d32a42cd584e5325b361d6639f28
rverma-nikiai <rohit.verma@niki.ai>
2019-06-26 18:10:58 +0530

    upgraded 0.12

--- private.tf
+++ private.tf
@@ -4,7 +4,7 @@ locals {
 }
 
 module "private_label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.13.0"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.14.1"
   namespace  = var.namespace
   name       = var.name
   stage      = var.stage
