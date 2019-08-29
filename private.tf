bf1fb56cd7ffc06f2c03bf36975af1ad0dc091f3
Soeren Malchow <soeren.malchow@mcon-group.com>
2019-08-29 16:40:46 +0200

    0.12upgrade, change to version 0.15.0 of terraform_null_label, changed list(string) to list for acls in variables.tf

--- private.tf
+++ private.tf
@@ -1,74 +1,116 @@
 locals {
-  private_count       = "${var.enabled == "true" && var.type == "private" ? length(var.availability_zones) : 0}"
-  private_route_count = "${var.enabled == "true" && var.type == "private" ? var.az_ngw_count : 0}"
+  private_count       = var.enabled == "true" && var.type == "private" ? length(var.availability_zones) : 0
+  private_route_count = var.enabled == "true" && var.type == "private" ? var.az_ngw_count : 0
 }
 
 module "private_label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.0"
-  namespace  = "${var.namespace}"
-  name       = "${var.name}"
-  stage      = "${var.stage}"
-  delimiter  = "${var.delimiter}"
-  tags       = "${var.tags}"
-  attributes = ["${compact(concat(var.attributes, list("private")))}"]
-  enabled    = "${var.enabled}"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.15.0"
+  namespace  = var.namespace
+  name       = var.name
+  stage      = var.stage
+  delimiter  = var.delimiter
+  tags       = var.tags
+  attributes = [compact(concat(var.attributes, ["private"]))]
+  enabled    = var.enabled
 }
 
 resource "aws_subnet" "private" {
-  count             = "${local.private_count}"
-  vpc_id            = "${var.vpc_id}"
-  availability_zone = "${element(var.availability_zones, count.index)}"
-  cidr_block        = "${cidrsubnet(var.cidr_block, ceil(log(var.max_subnets, 2)), count.index)}"
+  count             = local.private_count
+  vpc_id            = var.vpc_id
+  availability_zone = element(var.availability_zones, count.index)
+  cidr_block        = cidrsubnet(var.cidr_block, ceil(log(var.max_subnets, 2)), count.index)
 
-  tags = "${
-    merge(
-      module.private_label.tags,
-      map(
-        "Name", "${module.private_label.id}${var.delimiter}${element(var.availability_zones, count.index)}",
-        "AZ", "${element(var.availability_zones, count.index)}",
-        "Type", "${var.type}"
-      )
-    )
-  }"
+  tags = merge(
+    module.private_label.tags,
+    {
+      "Name" = "${module.private_label.id}${var.delimiter}${element(var.availability_zones, count.index)}"
+      "AZ"   = element(var.availability_zones, count.index)
+      "Type" = var.type
+    },
+  )
 }
 
 resource "aws_network_acl" "private" {
-  count      = "${var.enabled == "true" && var.type == "private" && signum(length(var.private_network_acl_id)) == 0 ? 1 : 0}"
-  vpc_id     = "${var.vpc_id}"
-  subnet_ids = ["${aws_subnet.private.*.id}"]
-  egress     = "${var.private_network_acl_egress}"
-  ingress    = "${var.private_network_acl_ingress}"
-  tags       = "${module.private_label.tags}"
-  depends_on = ["aws_subnet.private"]
+  count      = var.enabled == "true" && var.type == "private" && signum(length(var.private_network_acl_id)) == 0 ? 1 : 0
+  vpc_id     = var.vpc_id
+  subnet_ids = aws_subnet.private.*.id
+  dynamic "egress" {
+    for_each = var.private_network_acl_egress
+    content {
+      # TF-UPGRADE-TODO: The automatic upgrade tool can't predict
+      # which keys might be set in maps assigned here, so it has
+      # produced a comprehensive set here. Consider simplifying
+      # this after confirming which keys can be set in practice.
+
+      action          = lookup(egress.value, "action", null)
+      cidr_block      = lookup(egress.value, "cidr_block", null)
+      from_port       = lookup(egress.value, "from_port", null)
+      icmp_code       = lookup(egress.value, "icmp_code", null)
+      icmp_type       = lookup(egress.value, "icmp_type", null)
+      ipv6_cidr_block = lookup(egress.value, "ipv6_cidr_block", null)
+      protocol        = lookup(egress.value, "protocol", null)
+      rule_no         = lookup(egress.value, "rule_no", null)
+      to_port         = lookup(egress.value, "to_port", null)
+    }
+  }
+  dynamic "ingress" {
+    for_each = var.private_network_acl_ingress
+    content {
+      # TF-UPGRADE-TODO: The automatic upgrade tool can't predict
+      # which keys might be set in maps assigned here, so it has
+      # produced a comprehensive set here. Consider simplifying
+      # this after confirming which keys can be set in practice.
+
+      action          = lookup(ingress.value, "action", null)
+      cidr_block      = lookup(ingress.value, "cidr_block", null)
+      from_port       = lookup(ingress.value, "from_port", null)
+      icmp_code       = lookup(ingress.value, "icmp_code", null)
+      icmp_type       = lookup(ingress.value, "icmp_type", null)
+      ipv6_cidr_block = lookup(ingress.value, "ipv6_cidr_block", null)
+      protocol        = lookup(ingress.value, "protocol", null)
+      rule_no         = lookup(ingress.value, "rule_no", null)
+      to_port         = lookup(ingress.value, "to_port", null)
+    }
+  }
+  tags       = module.private_label.tags
+  depends_on = [aws_subnet.private]
 }
 
 resource "aws_route_table" "private" {
-  count  = "${local.private_count}"
-  vpc_id = "${var.vpc_id}"
+  count  = local.private_count
+  vpc_id = var.vpc_id
 
-  tags = "${
-    merge(
-     module.private_label.tags,
-     map(
-        "Name", "${module.private_label.id}${var.delimiter}${element(var.availability_zones, count.index)}",
-        "AZ", "${element(var.availability_zones, count.index)}",
-        "Type", "${var.type}"
-      )
-    )
-  }"
+  tags = merge(
+    module.private_label.tags,
+    {
+      "Name" = "${module.private_label.id}${var.delimiter}${element(var.availability_zones, count.index)}"
+      "AZ"   = element(var.availability_zones, count.index)
+      "Type" = var.type
+    },
+  )
 }
 
 resource "aws_route_table_association" "private" {
-  count          = "${local.private_count}"
-  subnet_id      = "${element(aws_subnet.private.*.id, count.index)}"
-  route_table_id = "${element(aws_route_table.private.*.id, count.index)}"
-  depends_on     = ["aws_subnet.private", "aws_route_table.private"]
+  count          = local.private_count
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+  route_table_id = element(aws_route_table.private.*.id, count.index)
+  depends_on = [
+    aws_subnet.private,
+    aws_route_table.private,
+  ]
 }
 
 resource "aws_route" "default" {
-  count                  = "${local.private_route_count}"
-  route_table_id         = "${lookup(zipmap(var.availability_zones, matchkeys(aws_route_table.private.*.id, aws_route_table.private.*.tags.AZ, var.availability_zones)), element(keys(var.az_ngw_ids), count.index))}"
-  nat_gateway_id         = "${lookup(var.az_ngw_ids, element(keys(var.az_ngw_ids), count.index))}"
+  count = local.private_route_count
+  route_table_id = zipmap(
+    var.availability_zones,
+    matchkeys(
+      aws_route_table.private.*.id,
+      aws_route_table.private.*.tags.AZ,
+      var.availability_zones,
+    ),
+  )[element(keys(var.az_ngw_ids), count.index)]
+  nat_gateway_id         = var.az_ngw_ids[element(keys(var.az_ngw_ids), count.index)]
   destination_cidr_block = "0.0.0.0/0"
-  depends_on             = ["aws_route_table.private"]
+  depends_on             = [aws_route_table.private]
 }
