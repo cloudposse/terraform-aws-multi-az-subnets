3cd74ba1fa7c108e5e12eeb8f8596d8a12bf76b2
Maxim Mironenko <maxim@cloudposse.com>
2019-10-11 17:48:34 +0600

    tests fix using fork branch

--- test/src/examples_complete_test.go
+++ test/src/examples_complete_test.go
@@ -7,6 +7,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func getKeys(m map[string]string) []string {
+  keys := make([]string, 0, len(m))
+  for k := range m {
+    keys = append(keys, k)
+  }
+  return keys
+}
+
 // Test the Terraform module in examples/complete using Terratest.
 func TestExamplesComplete(t *testing.T) {
 	t.Parallel()
@@ -26,16 +34,21 @@ func TestExamplesComplete(t *testing.T) {
 	terraform.InitAndApply(t, terraformOptions)
 
 	// Run `terraform output` to get the value of an output variable
-	privateSubnetIds := terraform.OutputList(t, terraformOptions, "private_az_subnet_ids")
+	privateSubnetIds := terraform.OutputMap(t, terraformOptions, "private_az_subnet_ids")
+	// Run `terraform output` to get the value of an output variable
+	privateRouteTableIds := terraform.OutputMap(t, terraformOptions, "private_az_route_table_ids")
+	// Run `terraform output` to get the value of an output variable
+	publicNATGateWayIds := terraform.OutputMap(t, terraformOptions, "public_az_ngw_ids")
+	// Run `terraform output` to get the value of an output variable
+	publicRouteTableIds := terraform.OutputMap(t, terraformOptions, "public_az_route_table_ids")
+	// Run `terraform output` to get the value of an output variable
+	publicSubnetIds := terraform.OutputMap(t, terraformOptions, "public_az_subnet_ids")
 
-// 	expectedPrivateSubnetCidrs := []string{"172.16.0.0/18", "172.16.64.0/18"}
+  expectedAZs := []string {"us-west-2a", "us-west-2b", "us-west-2c"}
 	// Verify we're getting back the outputs we expect
-	assert.Equal(t, 3, len(privateSubnetIds))
-
-// 	// Run `terraform output` to get the value of an output variable
-// 	publicSubnetCidrs := terraform.OutputList(t, terraformOptions, "public_subnet_cidrs")
-//
-// 	expectedPublicSubnetCidrs := []string{"172.16.128.0/18", "172.16.192.0/18"}
-// 	// Verify we're getting back the outputs we expect
-// 	assert.Equal(t, expectedPublicSubnetCidrs, publicSubnetCidrs)
+  assert.Equal(t, expectedAZs, getKeys(privateSubnetIds))
+  assert.Equal(t, expectedAZs, getKeys(privateRouteTableIds))
+  assert.Equal(t, expectedAZs, getKeys(publicNATGateWayIds))
+  assert.Equal(t, expectedAZs, getKeys(publicRouteTableIds))
+  assert.Equal(t, expectedAZs, getKeys(publicSubnetIds))
 }
