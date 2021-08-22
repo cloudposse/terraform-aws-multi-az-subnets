df64e3bff4ef292e3613fb14732a5ca4c2b79e3a
Naden <naden@mac.com>
2021-08-22 21:14:59 +1000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -135,5 +135,6 @@ variable "ipv6_cidr_block" {
 
 variable "assign_public_ip_on_launch" {
   description = "Flag to enable/disable whether instances launched into public subnets should be assigned a public IP address"
-  default     = "false"
+  type        = bool
+  default     = false
 }
