c7da7d562d17956bdb64ddf5924284a3c501ea01
Max Schwenk <maschwenk@gmail.com>
2023-06-27 17:50:18 +0000

    Add auto assign IP at launch

--- public.tf
+++ public.tf
@@ -18,12 +18,10 @@ module "public_label" {
 resource "aws_subnet" "public" {
   for_each = local.public_azs
 
-  vpc_id            = var.vpc_id
-  availability_zone = each.key
-  cidr_block        = cidrsubnet(var.cidr_block, ceil(log(var.max_subnets, 2)), each.value)
-  ipv6_cidr_block = local.public_ipv6_enabled ? cidrsubnet(var.ipv6_cidr_block, (
-    local.public_ipv6_target_mask - tonumber(split("/", var.ipv6_cidr_block)[1])
-  ), each.value) : null
+  vpc_id                  = var.vpc_id
+  availability_zone       = each.key
+  cidr_block              = cidrsubnet(var.cidr_block, ceil(log(var.max_subnets, 2)), each.value)
+  map_public_ip_on_launch = var.assign_public_ip_on_launch
 
   tags = merge(
     module.public_label.tags,
